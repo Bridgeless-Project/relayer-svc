e92ee28363fb61c16da0e246a4436b9cb475b683
EduardMikhrin <emihrin50@gmail.com>
2025-10-09 12:02:27 +0300

    added db

---
+++ internal/db/deposits.go
@@ -0,0 +1,50 @@
+package db
+
+import (
+	"github.com/Bridgeless-Project/relayer-svc/internal/types"
+	"github.com/pkg/errors"
+)
+
+type DepositsQ interface {
+	New() DepositsQ
+	Insert(Deposit) (id int64, err error)
+	Get(identifier DepositIdentifier) (*Deposit, error)
+
+	UpdateWithdrawalTx(DepositIdentifier, string) error
+	UpdateSignature(DepositIdentifier, string) error
+	UpdateStatus(DepositIdentifier, types.WithdrawalStatus) error
+
+	Transaction(f func() error) error
+}
+
+var ErrAlreadySubmitted = errors.New("transaction already submitted")
+
+type DepositIdentifier struct {
+	TxHash  string `structs:"tx_hash" db:"tx_hash"`
+	TxNonce int64  `structs:"tx_nonce" db:"tx_nonce"`
+	ChainId string `structs:"chain_id" db:"chain_id"`
+}
+
+type Deposit struct {
+	Id int64 `structs:"-" db:"id"`
+	DepositIdentifier
+
+	Depositor        *string `structs:"depositor" db:"depositor"`
+	DepositAmount    string  `structs:"deposit_amount" db:"deposit_amount"`
+	DepositToken     string  `structs:"deposit_token" db:"deposit_token"`
+	Receiver         string  `structs:"receiver" db:"receiver"`
+	WithdrawalToken  string  `structs:"withdrawal_token" db:"withdrawal_token"`
+	DepositBlock     int64   `structs:"deposit_block" db:"deposit_block"`
+	CommissionAmount string  `structs:"commission_amount" db:"commission_amount"`
+	ReferralId       uint16  `structs:"referral_id" db:"referral_id"`
+
+	WithdrawalStatus types.WithdrawalStatus `structs:"withdrawal_status" db:"withdrawal_status"`
+
+	WithdrawalTxHash  *string `structs:"withdrawal_tx_hash" db:"withdrawal_tx_hash"`
+	WithdrawalChainId string  `structs:"withdrawal_chain_id" db:"withdrawal_chain_id"`
+	WithdrawalAmount  string  `structs:"withdrawal_amount" db:"withdrawal_amount"`
+
+	IsWrappedToken bool `structs:"is_wrapped_token" db:"is_wrapped_token"`
+
+	Signature *string `structs:"signature" db:"signature"`
+}
---
+++ internal/db/postgres/deposits.go
@@ -0,0 +1,187 @@
+package pg
+
+import (
+	"database/sql"
+	"strings"
+
+	"github.com/Bridgeless-Project/relayer-svc/internal/db"
+	"github.com/Bridgeless-Project/relayer-svc/internal/types"
+	"github.com/Masterminds/squirrel"
+	"github.com/pkg/errors"
+	"gitlab.com/distributed_lab/kit/pgdb"
+)
+
+const (
+	depositsTable   = "deposits"
+	depositsTxHash  = "tx_hash"
+	depositsTxNonce = "tx_nonce"
+	depositsChainId = "chain_id"
+	depositsId      = "id"
+
+	depositsDepositor        = "depositor"
+	depositsDepositAmount    = "deposit_amount"
+	depositsWithdrawalAmount = "withdrawal_amount"
+	depositsDepositToken     = "deposit_token"
+	depositsReceiver         = "receiver"
+	depositsWithdrawalToken  = "withdrawal_token"
+	depositsDepositBlock     = "deposit_block"
+	depositsReferralId       = "referral_id"
+
+	depositsWithdrawalChainId = "withdrawal_chain_id"
+	depositsWithdrawalTxHash  = "withdrawal_tx_hash"
+
+	depositsWithdrawalStatus = "withdrawal_status"
+
+	depositsIsWrappedToken   = "is_wrapped_token"
+	depositsCommissionAmount = "commission_amount"
+
+	depositsSignature = "signature"
+)
+
+type depositsQ struct {
+	db       *pgdb.DB
+	selector squirrel.SelectBuilder
+}
+
+func (d *depositsQ) New() db.DepositsQ {
+	return NewDepositsQ(d.db.Clone())
+}
+
+func (d *depositsQ) Insert(deposit db.Deposit) (int64, error) {
+	stmt := squirrel.
+		Insert(depositsTable).
+		SetMap(map[string]interface{}{
+			depositsTxHash:           deposit.TxHash,
+			depositsTxNonce:          deposit.TxNonce,
+			depositsChainId:          deposit.ChainId,
+			depositsWithdrawalStatus: deposit.WithdrawalStatus,
+			depositsDepositAmount:    deposit.DepositAmount,
+			depositsWithdrawalAmount: deposit.WithdrawalAmount,
+			depositsReceiver:         deposit.Receiver,
+			depositsDepositBlock:     deposit.DepositBlock,
+			depositsIsWrappedToken:   deposit.IsWrappedToken,
+			// can be 0x00... in case of native ones
+			depositsDepositToken: deposit.DepositToken,
+			depositsDepositor:    deposit.Depositor,
+			// can be 0x00... in case of native ones
+			depositsWithdrawalToken:   deposit.WithdrawalToken,
+			depositsWithdrawalChainId: deposit.WithdrawalChainId,
+			depositsCommissionAmount:  deposit.CommissionAmount,
+			depositsReferralId:        deposit.ReferralId,
+		}).
+		Suffix("RETURNING id")
+
+	var id int64
+	if err := d.db.Get(&id, stmt); err != nil {
+		if strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+			err = db.ErrAlreadySubmitted
+		}
+
+		return id, err
+	}
+
+	return id, nil
+}
+
+func (d *depositsQ) Get(identifier db.DepositIdentifier) (*db.Deposit, error) {
+	var deposit db.Deposit
+	err := d.db.Get(&deposit, d.selector.Where(identifierToPredicate(identifier)))
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, nil
+	}
+
+	return &deposit, err
+}
+
+func identifierToPredicate(identifier db.DepositIdentifier) squirrel.Eq {
+	return squirrel.Eq{
+		depositsTxHash:  identifier.TxHash,
+		depositsTxNonce: identifier.TxNonce,
+		depositsChainId: identifier.ChainId,
+	}
+}
+
+func (d *depositsQ) UpdateWithdrawalDetails(identifier db.DepositIdentifier, hash *string, signature *string) error {
+	query := squirrel.Update(depositsTable).
+		Set(depositsWithdrawalTxHash, hash).
+		Set(depositsSignature, signature).
+		Set(depositsWithdrawalStatus, types.WithdrawalStatus_WITHDRAWAL_STATUS_PROCESSED).
+		Where(identifierToPredicate(identifier))
+
+	return d.db.Exec(query)
+}
+
+func (d *depositsQ) UpdateSignature(identifier db.DepositIdentifier, sig string) error {
+	query := squirrel.Update(depositsTable).
+		Set(depositsWithdrawalStatus, types.WithdrawalStatus_WITHDRAWAL_STATUS_PROCESSED).
+		Set(depositsSignature, sig).
+		Where(identifierToPredicate(identifier))
+
+	return d.db.Exec(query)
+}
+
+func (d *depositsQ) UpdateStatus(identifier db.DepositIdentifier, status types.WithdrawalStatus) error {
+	query := squirrel.Update(depositsTable).
+		Set(depositsWithdrawalStatus, status).
+		Where(identifierToPredicate(identifier))
+
+	return d.db.Exec(query)
+}
+
+func (d *depositsQ) UpdateWithdrawalTx(identifier db.DepositIdentifier, hash string) error {
+	query := squirrel.Update(depositsTable).
+		Set(depositsWithdrawalTxHash, hash).
+		Set(depositsWithdrawalStatus, types.WithdrawalStatus_WITHDRAWAL_STATUS_PROCESSED).
+		Where(identifierToPredicate(identifier))
+
+	return d.db.Exec(query)
+}
+
+func NewDepositsQ(db *pgdb.DB) db.DepositsQ {
+	return &depositsQ{
+		db:       db.Clone(),
+		selector: squirrel.Select("*").From(depositsTable),
+	}
+}
+
+func (d *depositsQ) Transaction(f func() error) error {
+	return d.db.Transaction(f)
+}
+
+func (d *depositsQ) InsertProcessedDeposit(deposit db.Deposit) (int64, error) {
+	stmt := squirrel.
+		Insert(depositsTable).
+		SetMap(map[string]interface{}{
+			depositsTxHash:           deposit.TxHash,
+			depositsTxNonce:          deposit.TxNonce,
+			depositsChainId:          deposit.ChainId,
+			depositsDepositAmount:    deposit.DepositAmount,
+			depositsWithdrawalAmount: deposit.WithdrawalAmount,
+			depositsCommissionAmount: deposit.CommissionAmount,
+			depositsReceiver:         strings.ToLower(deposit.Receiver),
+			depositsDepositBlock:     deposit.DepositBlock,
+			depositsIsWrappedToken:   deposit.IsWrappedToken,
+			// can be 0x00... in case of native ones
+			depositsDepositToken: strings.ToLower(deposit.DepositToken),
+			depositsDepositor:    deposit.Depositor,
+			// can be 0x00... in case of native ones
+			depositsWithdrawalToken:   strings.ToLower(deposit.WithdrawalToken),
+			depositsWithdrawalChainId: deposit.WithdrawalChainId,
+			depositsWithdrawalTxHash:  deposit.WithdrawalTxHash,
+			depositsSignature:         deposit.Signature,
+			depositsWithdrawalStatus:  types.WithdrawalStatus_WITHDRAWAL_STATUS_PROCESSED,
+			depositsReferralId:        deposit.ReferralId,
+		}).
+		Suffix("RETURNING id")
+
+	var id int64
+	if err := d.db.Get(&id, stmt); err != nil {
+		if strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+			err = db.ErrAlreadySubmitted
+		}
+
+		return id, err
+	}
+
+	return id, nil
+}
