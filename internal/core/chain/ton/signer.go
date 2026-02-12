503a3a8a5378ea84cbd08d0cd3fc6cb70f02b6d7
Andrii Bialkovkyi <maphy9@gmail.com>
2026-02-12 13:15:51 +0100

    Add pubkey fetching

--- internal/core/chain/ton/signer.go
+++ internal/core/chain/ton/signer.go
@@ -2,8 +2,13 @@ package ton
 
 import (
 	"context"
+	"crypto/ecdh"
+	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
 	"math/big"
+	"net/http"
 
 	"github.com/Bridgeless-Project/relayer-svc/internal/db"
 	"github.com/pkg/errors"
@@ -49,11 +54,16 @@ func (c *Client) buildUpdateSignerCell(epochData *db.Epoch) (*cell.Cell, error) 
 		return nil, errors.Wrap(err, "error getting signature cell")
 	}
 
-	// TODO: Replace the key with actual values
+	var h uint64 = 0x04
+	x, y, err := getPubkey(epochData.Id)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get pubkey")
+	}
+
 	cell0 := cell.BeginCell().
-		MustStoreUInt(1, 8).
-		MustStoreBigUInt(big.NewInt(2), 256).
-		MustStoreBigUInt(big.NewInt(3), 256)
+		MustStoreUInt(h, 8).
+		MustStoreBigUInt(x, 256).
+		MustStoreBigUInt(y, 256)
 
 	cell1 := cell.BeginCell().
 		MustStoreUInt(epochData.StartTime, 64).
@@ -75,10 +85,11 @@ func (c *Client) getUpdateSignerHash(ctx context.Context, epoch *db.Epoch) ([]by
 		return nil, errors.Wrap(err, "failed to get the master chain info")
 	}
 
-	// TODO: Replace the key with actual values
-	h := big.NewInt(1)
-	x := big.NewInt(2)
-	y := big.NewInt(3)
+	var h uint64 = 0x04
+	x, y, err := getPubkey(epoch.Id)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get pubkey")
+	}
 
 	res, err := c.Chain.Client.WaitForBlock(master.SeqNo).RunGetMethod(
 		ctx,
@@ -102,4 +113,31 @@ func (c *Client) getUpdateSignerHash(ctx context.Context, epoch *db.Epoch) ([]by
 	}
 
 	return resBig.Bytes(), nil
+}
+
+type getPubkeyResponse struct {
+	Pubkey string `json:"pub_key"`
+}
+
+func getPubkey(epochId uint32) (*big.Int, *big.Int, error) {
+	// TODO: replace hardcoded url with API url from config
+	url := fmt.Sprintf("http://localhost:1317​cosmos​/bridge​/epoch​/​%d/pubkey", epochId)
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer res.Body.Close()
+
+	var resStruct getPubkeyResponse
+	if err := json.NewDecoder(res.Body).Decode(&resStruct); err != nil {
+		return nil, nil, err
+	}
+
+	privKeyBytes := sha256.Sum256([]byte(resStruct.Pubkey))
+	privKey, err := ecdh.P256().NewPrivateKey(privKeyBytes[:])
+	pubKey := privKey.PublicKey()
+	pubKeyBytes := pubKey.Bytes()
+	x := new(big.Int).SetBytes(pubKeyBytes[1:33])
+	y := new(big.Int).SetBytes(pubKeyBytes[33:65])
+	return x, y, nil
 }
