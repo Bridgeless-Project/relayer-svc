c12b55b750eb02f5e8fd2687bd99db32d4a80460
Andrii Bialkovkyi <maphy9@gmail.com>
2026-02-16 12:04:46 +0100

    confirm evm signature working

--- internal/core/chain/evm/signer.go
+++ internal/core/chain/evm/signer.go
@@ -2,9 +2,11 @@ package evm
 
 import (
 	"context"
+	"fmt"
 	"math/big"
 
 	"github.com/Bridgeless-Project/relayer-svc/internal/db"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/pkg/errors"
@@ -54,6 +56,19 @@ func (c *Client) UpdateSigners(ctx context.Context, epochData *db.Epoch, signer 
 	if err != nil {
 		return "", 0, errors.Wrap(err, "failed to update signers")
 	}
+
+	receipt, err := bind.WaitMined(ctx, c.chain.Rpc, tx)
+	if err != nil {
+		return "", 0, errors.Wrap(err, "failed to wait for mining")
+	}
+
+	if receipt.Status != 1 {
+    return "", 0, fmt.Errorf("transaction execution failed")
+	}
+
+	if receipt.GasUsed < 30000 {
+		return "", 0, fmt.Errorf("silent failure: gas used (%d) too low for state change", receipt.GasUsed)
+  }
 
 	return tx.Hash().Hex(), block, nil
 }
