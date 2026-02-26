edadb4f6a5a53704ca32c7cd6b74b7d4fd9958ce
Andrii Bialkovkyi <maphy9@gmail.com>
2026-02-26 14:10:35 +0100

    add basic update signers call for ton

--- internal/core/chain/ton/signer.go
+++ internal/core/chain/ton/signer.go
@@ -2,142 +2,97 @@ package ton
 
 import (
 	"context"
-	"crypto/ecdh"
-	"crypto/sha256"
 	"encoding/hex"
-	"encoding/json"
-	"fmt"
+	"log"
 	"math/big"
-	"net/http"
+	"strings"
 
 	"github.com/Bridgeless-Project/relayer-svc/internal/db"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/pkg/errors"
 	"github.com/xssnick/tonutils-go/tlb"
 	"github.com/xssnick/tonutils-go/ton/wallet"
 	"github.com/xssnick/tonutils-go/tvm/cell"
 )
 
-func (c *Client) updateSigner(ctx context.Context, epochData *db.Epoch, signer *wallet.Wallet) (string, int64, error) {
-	ctxt := c.Chain.Client.Client().StickyContext(ctx)
-	updateSignerCell, err := c.buildUpdateSignerCell(epochData)
-	if err != nil {
-		return "", 0, errors.Wrap(err, "failed to build update signer cell")
-	}
+func (c *Client) UpdateSigners(ctx context.Context, epochData *db.Epoch, signer *wallet.Wallet) (string, int64, error) {
+  ctxt := c.Chain.Client.Client().StickyContext(ctx)
 
-	b, err := c.Chain.Client.GetMasterchainInfo(ctxt)
-	if err != nil {
-		return "", 0, errors.Wrap(err, "failed to get master chain info")
-	}
+  log.Printf("UpdateSigners | target_addr=%s epoch_id=%d", c.Chain.BridgeContractAddress.String(), epochData.Id)
 
-	bytes, err := signer.SendManyWaitTxHash(ctx, []*wallet.Message{
-		{
-			Mode: 1,
-			InternalMessage: &tlb.InternalMessage{
-				IHRDisabled: true,
-				Bounce:      false,
-				DstAddr:     c.Chain.BridgeContractAddress,
-				Amount:      tlb.FromNanoTONU(1500000000),
-				Body:        updateSignerCell,
-			},
-		},
-	})
-	if err != nil {
-		return "", 0, errors.Wrap(err, "error sending withdrawal transaction")
-	}
+  updateSignerCell, err := c.buildUpdateSignerCell(epochData)
+  if err != nil {
+    return "", 0, errors.Wrap(err, "failed to build update signer cell")
+  }
 
-	return hex.EncodeToString(bytes), int64(b.SeqNo), errors.Wrapf(err, "failed to withdraw native")
+  log.Printf("UpdateSigners | cell_hash=%x", updateSignerCell.Hash())
+
+  b, err := c.Chain.Client.GetMasterchainInfo(ctxt)
+  if err != nil {
+    return "", 0, errors.Wrap(err, "failed to get master chain info")
+  }
+
+  txHashBytes, err := signer.SendManyWaitTxHash(ctx, []*wallet.Message{
+    {
+      Mode: 1,
+      InternalMessage: &tlb.InternalMessage{
+        IHRDisabled: true,
+        Bounce:      true,
+        DstAddr:     c.Chain.BridgeContractAddress,
+        Amount:      tlb.FromNanoTONU(200000000),
+        Body:        updateSignerCell,
+      },
+    },
+  })
+  if err != nil {
+    return "", 0, errors.Wrap(err, "error sending withdrawal transaction")
+  }
+
+  log.Printf("UpdateSigners | tx_hash=%x seqno=%d", txHashBytes, b.SeqNo)
+  return hex.EncodeToString(txHashBytes), int64(b.SeqNo), nil
 }
 
 func (c *Client) buildUpdateSignerCell(epochData *db.Epoch) (*cell.Cell, error) {
-	signatureCell, err := getSignatureCell(epochData.Signature)
-	if err != nil {
-		return nil, errors.Wrap(err, "error getting signature cell")
-	}
-
-	var h uint64 = 0x04
-	x, y, err := getPubkey(epochData.Id)
+	x, y, err := getPubkey(epochData.Signer)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get pubkey")
 	}
 
-	cell0 := cell.BeginCell().
-		MustStoreUInt(h, 8).
-		MustStoreBigUInt(x, 256).
-		MustStoreBigUInt(y, 256)
+	rawSig := strings.TrimPrefix(epochData.Signature, "0x")
+	signatureBytes, err := hex.DecodeString(rawSig)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to decode signature")
+	}
 
-	cell1 := cell.BeginCell().
-		MustStoreUInt(epochData.StartTime, 64).
-		MustStoreUInt(epochData.EndTime, 64).
-		MustStoreBoolBit(epochData.SignatureMode).
-		MustStoreRef(signatureCell).
+	signatureCell := cell.BeginCell().
+		MustStoreSlice(signatureBytes, uint(len(signatureBytes)*8)).
 		EndCell()
 
 	body := cell.BeginCell().
-		MustStoreBuilder(cell0.MustStoreRef(cell1)).
+		MustStoreUInt(0x12312324, 32).
+		MustStoreUInt(4, 8).
+		MustStoreBigUInt(x, 256).
+		MustStoreBigUInt(y, 256).
+		MustStoreUInt(epochData.StartTime, 32).
+		MustStoreUInt(epochData.EndTime, 32).
+		MustStoreBoolBit(epochData.SignatureMode).
+		MustStoreRef(signatureCell).
 		EndCell()
 
 	return body, nil
 }
 
-func (c *Client) getUpdateSignerHash(ctx context.Context, epoch *db.Epoch) ([]byte, error) {
-	master, err := c.Chain.Client.GetMasterchainInfo(ctx)
+func getPubkey(pubkeyHex string) (*big.Int, *big.Int, error) {
+	cleanHex := strings.TrimPrefix(pubkeyHex, "0x")
+	pubKeyBytes, err := hex.DecodeString(cleanHex)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to get the master chain info")
+		return nil, nil, errors.Wrap(err, "invalid pubkey hex")
 	}
 
-	var h uint64 = 0x04
-	x, y, err := getPubkey(epoch.Id)
+	pubTON, err := crypto.UnmarshalPubkey(pubKeyBytes)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to get pubkey")
+		return nil, nil, errors.Wrap(err, "failed to unmarshal uncompressed pubkey")
 	}
 
-	res, err := c.Chain.Client.WaitForBlock(master.SeqNo).RunGetMethod(
-		ctx,
-		master,
-		c.Chain.BridgeContractAddress,
-		updateSignerHashMethod,
-		h,
-		x,
-		y,
-		epoch.StartTime,
-		epoch.EndTime,
-		epoch.SignatureMode,
-	)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get the native hash")
-	}
-
-	resBig, err := res.Int(0)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get the withdrawal native hash")
-	}
-
-	return resBig.Bytes(), nil
-}
-
-type getPubkeyResponse struct {
-	Pubkey string `json:"pub_key"`
-}
-
-func getPubkey(epochId uint32) (*big.Int, *big.Int, error) {
-	// TODO: replace hardcoded url with API url from config
-	url := fmt.Sprintf("http://localhost:1317​cosmos​/bridge​/epoch​/​%d/pubkey", epochId)
-	res, err := http.Get(url)
-	if err != nil {
-		return nil, nil, err
-	}
-	defer res.Body.Close()
-
-	var resStruct getPubkeyResponse
-	if err := json.NewDecoder(res.Body).Decode(&resStruct); err != nil {
-		return nil, nil, err
-	}
-
-	privKeyBytes := sha256.Sum256([]byte(resStruct.Pubkey))
-	privKey, err := ecdh.P256().NewPrivateKey(privKeyBytes[:])
-	pubKey := privKey.PublicKey()
-	pubKeyBytes := pubKey.Bytes()
-	x := new(big.Int).SetBytes(pubKeyBytes[1:33])
-	y := new(big.Int).SetBytes(pubKeyBytes[33:65])
-	return x, y, nil
+	return pubTON.X, pubTON.Y, nil
 }
